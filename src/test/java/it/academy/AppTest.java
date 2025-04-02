f98a1f8bdafe26971e388026a166ab4c8f468066
Sergi Virgili <svirgilif@gmail.com>
2025-04-02 16:42:38 +0200

    test: add initial unit test for application functionality

---
+++ src/test/java/it/academy/AppTest.java
@@ -0,0 +1,13 @@
+package it.academy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void appShouldRun() {
+        System.out.println("Application is running...");
+        assertTrue(true);
+    }
+}
